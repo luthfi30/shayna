c4559503670f87a62f401a223b187af4b4b2999d
luthfi30 <lurrah30@gmail.com>
2020-03-19 13:02:13 +0700

    upload project vue

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna/>
+    <WomenShayna/>
+    <InstaShayna/>
+    <LogoShayna/>
+    <FooterShayna/>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroShayna from '@/components/HeroShayna.vue'
+import WomenShayna from '@/components/WomenShayna.vue'
+import InstaShayna from '@/components/InstaShayna.vue'
+import LogoShayna from '@/components/LogoShayna.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
+export default {
+  name: 'home',
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomenShayna,
+    InstaShayna,
+    LogoShayna,
+    FooterShayna
+  }
+}
+</script>
